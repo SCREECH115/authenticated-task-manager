df688bcc331490ddca046cc12ff3ba658efc0312
SCREECH115 <arekw99@gmail.com>
2023-04-23 14:04:27 +0200

    Deployed app

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import ListHeader from "./components/ListHeader";
+import ListItem from "./components/ListItem";
+import Auth from "./components/Auth";
+import { useCookies } from "react-cookie";
+
+const App = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const authToken = cookies.AuthToken;
+  const userEmail = cookies.Email;
+  const [tasks, setTasks] = useState(null);
+
+  const getData = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`
+      );
+      const json = await response.json();
+      setTasks(json);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (authToken) {
+      getData();
+    }
+  }, []);
+  console.log(tasks);
+
+  // Sort tasks by date
+  const sortedTasks = tasks?.sort((a, b) => {
+    return new Date(a.date) - new Date(b.date);
+  });
+
+  return (
+    <div className="app">
+      {!authToken && <Auth />}
+      {authToken && (
+        <>
+          <ListHeader listName={"📓 Task Manager"} getData={getData} />
+          <p>Helo {userEmail}</p>
+          {sortedTasks?.map((task) => (
+            <ListItem key={task.id} task={task} getData={getData} />
+          ))}{" "}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
